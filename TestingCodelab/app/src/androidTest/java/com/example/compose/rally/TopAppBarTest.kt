0a74ae5ceb486edfbda852cc43e6bae864f910a4
EdwinLovo <edwinlovo.dev@gmail.com>
2022-05-03 12:03:05 -0600

    :tada: Testing codelab

---
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,60 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+    /**
+     * Bad news though: this test is not very useful! If you look at the Semantics tree closely,
+     * the content descriptions of all three tabs are there whether or not their tab is selected.
+     * We must go deeper!
+     *
+     * NOTE: already solved!
+     */
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
+}
